da7e4f4deb66959b011dc030c6db528b2934b4ff
GUILLERMO CAMACHO <guiller.camacho.jara@gmail.com>
2024-05-19 12:04:43 -0500

    Se instala bd atlas

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ app.get('/', (req, res) => {
     res.send('prueba 1 respuesta del servidor') // ruta default
 } );
 
+
+//conexion a la bd
+monggoose.connect('mongodb+srv://<username>:<password>@cluster0.ywdzkcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+{useNewParser: true }, () =>{
+    console.log('Si hay conexión a la BD');
+});
+
+
+
 // Se configura como va escuchar el servidor las peticiones
 
 app.listen(10000);
-
